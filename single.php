9c390fb785bdd6a620c60f327cb6f69787672c46
Al-Mothafar Al-Hasan <almothafar@gmail.com>
2025-11-24 21:15:20 +0300

    Trying to reduce complexity

--- single.php
+++ single.php
@@ -5,14 +5,14 @@
 		<?php while ( have_posts() ) : the_post(); ?>
 			<article class="post" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<div class="post-head">
-					<div class="post-time"><?php echo esc_html( get_the_date( 'j F Y' ) ); ?></div>
-					<div class="post-cat">التصنيف : <?php the_category( '، ' ); ?></div>
-					<div class="post-author">الكاتب : <?php the_author_posts_link(); ?></div>
-					<div class="post-comment">
+					<div class="post-head-stat post-time"><?php echo esc_html( get_the_date( 'j F Y' ) ); ?></div>
+					<div class="post-head-stat post-cat">التصنيف : <?php the_category( '، ' ); ?></div>
+					<div class="post-head-stat post-author">الكاتب : <?php the_author_posts_link(); ?></div>
+					<div class="post-head-stat post-comment">
 						<?php comments_popup_link( 'لا تعليقات', 'التعليقات : 1', 'التعليقات : %', '', 'التعليقات مغلقة لهذه التدوينة' ); ?>
 					</div>
 					<?php if ( function_exists( 'the_views' ) ) : ?>
-						<div class="post-view"><?php the_views(); ?></div>
+						<div class="post-head-stat post-view"><?php the_views(); ?></div>
 					<?php endif; ?>
 				</div>
 				<div class="entry">
